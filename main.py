ca82bcadeb3c34912c60a73df3416e35d72c17d4
Sliots <Sliots@hotmail.com>
2024-03-23 17:45:17 +0800

    增加 lastupdated, manifest 信息

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 import subprocess
-
+from datetime import datetime
 
 class SteamBackup:
     def __init__(self, steam_library, backup_dir, winrar_path, csv_file):
@@ -14,49 +14,67 @@ class SteamBackup:
     def read_backup_csv(self):
         backup_data = {}
         if os.path.exists(self.csv_file):
-            with open(self.csv_file, 'r') as file:
+            with open(self.csv_file, 'r', encoding='utf-8') as file:
                 reader = csv.reader(file)
                 for row in reader:
-                    appid, buildid = row
-                    backup_data[appid] = buildid
+                    appid, buildid, lastupdated, manifest = row
+                    backup_data[appid] = (buildid, lastupdated, manifest)
         return backup_data
 
     def write_backup_csv(self):
-        with open(self.csv_file, 'w', newline='') as file:
+        with open(self.csv_file, 'w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            for appid, buildid in self.backup_data.items():
-                writer.writerow([appid, buildid])
+            for appid, data in self.backup_data.items():
+                writer.writerow([appid, *data])
 
-    def backup_app(self, appid, installdir, buildid):
+    def backup_app(self, appid, installdir, buildid, lastupdated, manifest):
         app_dir = os.path.join(self.steam_library, "common", installdir)
-        backup_filename = f"[{appid}][{buildid}]{installdir}.rar"
+
+        lastupdated_date = datetime.fromtimestamp(int(lastupdated)).strftime('%y%m%d')
+
+        backup_filename = f"[{appid}][{buildid}][{lastupdated_date}][{manifest}]{installdir}.rar"
         backup_path = os.path.join(self.backup_dir, backup_filename)
 
-        # 压缩文件
-        subprocess.run([self.winrar_path, "a", "-EP1", "-IBCK", "-K", "-M5", "-MCX", "-MD4G", "-OI", "-S", backup_path, app_dir])
+        self.compress_files(app_dir, backup_path)
 
-        # 更新 backup.csv
-        self.backup_data[appid] = buildid
+        self.backup_data[appid] = (buildid, lastupdated, manifest)
         self.write_backup_csv()
 
         print(f"App {installdir}:{appid}:{buildid} backed up successfully.")
+
+    def compress_files(self, source, destination):
+        subprocess.run([self.winrar_path, "a", "-EP1", "-IBCK", "-K", "-M5", "-MCX", "-MD4G", "-OI", "-S", destination, source])
 
     def run_backup(self):
         for acf_file in os.listdir(self.steam_library):
             if acf_file.startswith("appmanifest_") and acf_file.endswith(".acf"):
                 acf_path = os.path.join(self.steam_library, acf_file)
-                with open(acf_path, 'r') as file:
-                    content = file.read()
-                    appid = content.split('"appid"\t\t"')[1].split('"')[0]
-                    installdir = content.split('"installdir"\t\t"')[1].split('"')[0]
-                    buildid = content.split('"buildid"\t\t"')[1].split('"')[0]
+                self.process_acf_file(acf_path)
 
-                    if appid in self.backup_data and self.backup_data[appid] == buildid:
-                        print(f"App {installdir}:{appid}:{buildid} already exists.")
-                        continue
+    def process_acf_file(self, acf_path):
+        with open(acf_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            appid = content.split('"appid"\t\t"')[1].split('"')[0]
+            installdir = content.split('"installdir"\t\t"')[1].split('"')[0]
+            buildid = content.split('"buildid"\t\t"')[1].split('"')[0]
+            lastupdated = content.split('"lastupdated"\t\t"')[1].split('"')[0]
 
-                    self.backup_app(appid, installdir, buildid)
+            depots_start = content.find('"InstalledDepots"')
+            if depots_start != -1:
+                depots_end = content.find('}', depots_start)
+                depots_content = content[depots_start:depots_end]
+                first_manifest = depots_content.split('"manifest"\t\t"')[1].split('"')[0]
 
+            if appid in self.backup_data and self.backup_data[appid][0] == buildid and self.backup_data[appid][1] == lastupdated and self.backup_data[appid][2] == first_manifest:
+                print(f"App {installdir}:{appid}:{buildid} already exists.")
+                return
+
+            self.backup_app(appid, installdir, buildid, lastupdated, first_manifest)
+
+class SteamBackupFactory:
+    @staticmethod
+    def create(steam_library, backup_dir, winrar_path, csv_file):
+        return SteamBackup(steam_library, backup_dir, winrar_path, csv_file)
 
 # 设置参数
 steam_library = r"D:\SteamLibrary\steamapps"
@@ -65,5 +83,5 @@ winrar_path = r"C:\Program Files\WinRAR\WinRAR.exe"
 csv_file = os.path.join(backup_dir, "backup.csv")
 
 # 创建 SteamBackup 实例并运行备份
-steam_backup = SteamBackup(steam_library, backup_dir, winrar_path, csv_file)
+steam_backup = SteamBackupFactory.create(steam_library, backup_dir, winrar_path, csv_file)
 steam_backup.run_backup()
